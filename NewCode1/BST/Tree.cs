fccb5e1575a7019e81abe4fa6840af0323e438ab
Ben Tyler <ben.tyler@rsmuk.com>
2020-08-07 15:27:14 +0100

    Initial Commit

---
+++ NewCode1/BST/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NewCode1.BST
+{
+    public class Tree<TKey, TValue>
+    {
+        public Node<TKey, TValue> Root { get; private set; }
+
+        public Node<TKey, TValue> AddNode(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue FindByKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
